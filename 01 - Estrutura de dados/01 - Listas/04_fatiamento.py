da568efe5d2880a5d3bb751cecdf2f7c81bdb850
walter-oliveira <walteroli@gmail.com>
2024-09-05 11:00:45 -0300

    novo exemplo

--- 01 - Estrutura de dados/01 - Listas/04_fatiamento.py
+++ 01 - Estrutura de dados/01 - Listas/04_fatiamento.py
@@ -5,4 +5,4 @@ print(lista[:2])  # ["p", "y"]
 print(lista[1:3])  # ["y", "t"]
 print(lista[0:3:2])  # ["p", "t"]
 print(lista[::])  # ["p", "y", "t", "h", "o", "n"]
-print(lista[::-1])  # ["n", "o", "h", "t", "y", "p"]
+print(lista[::-1])  # ["n", "o", "h", "t", "y", "p"] # espelhamento 
--- 01 - Estrutura de dados/01 - Listas/18_sorted.py
+++ 01 - Estrutura de dados/01 - Listas/18_sorted.py
@@ -2,3 +2,7 @@ linguagens = ["python", "js", "c", "java", "csharp"]
 
 print(sorted(linguagens, key=lambda x: len(x)))  # ["c", "js", "java", "python", "csharp"]
 print(sorted(linguagens, key=lambda x: len(x), reverse=True))  # ["python", "csharp", "java", "js", "c"]
+
+
+a = [n **2 if n>6 else n for n in range (10) if n%2==0]
+print (a)
